8cd1167ec5143de7dca2d02eb6cb9230350cc4e4
JJoaoVVitor <joaovitorbe3@gmail.com>
2026-06-09 14:27:27 -0300

    tela de perfil atualizada

--- lib/models/app_user.dart
+++ lib/models/app_user.dart
@@ -2,8 +2,9 @@ class AppUser {
   final String uid;
   final String name;
   final String email;
+  final String? photoUrl;
 
-  AppUser({required this.uid, required this.name, required this.email});
+  AppUser({required this.uid, required this.name, required this.email, this.photoUrl});
 
   Map<String, dynamic> toMap() {
     return {'uid': uid, 'name': name, 'email': email};
--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -27,5 +27,9 @@ class AuthService {
     await _auth.signOut();
   }
 
+  Future<void> sendPasswordResetEmail(String email) async {
+    await _auth.sendPasswordResetEmail(email: email);
+  }
+
   Stream<User?> get userChanges => _auth.userChanges();
 }
